1a28f5fd7ecbfe18c5268b3a1ee24dde44935245
descellia <descelliiaa@gmail.com>
2025-09-05 18:03:31 +0700

    Commit No.5 - Fix ResultActivity to allow resubmitting color code without app restart

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,10 +1,11 @@
 package com.example.lab_week_02_b
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -16,7 +17,6 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
         if (intent != null) {
@@ -24,25 +24,26 @@ class ResultActivity : AppCompatActivity() {
 
             val backgroundScreen =
                 findViewById<ConstraintLayout>(R.id.background_screen)
+            val resultMessage =
+                findViewById<TextView>(R.id.color_code_result_message)
 
             try {
-                backgroundScreen.setBackgroundColor(
-                    Color.parseColor("#$colorCode")
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+                resultMessage.text = getString(
+                    R.string.color_code_result_message,
+                    colorCode?.uppercase()
                 )
             } catch (ex: IllegalArgumentException) {
                 Intent().let { errorIntent ->
                     errorIntent.putExtra(ERROR_KEY, true)
-                    setResult(RESULT_OK, errorIntent)
+                    setResult(Activity.RESULT_OK, errorIntent)
                     finish()
                 }
             }
-
-            val resultMessage =
-                findViewById<TextView>(R.id.color_code_result_message)
-            resultMessage.text = getString(
-                R.string.color_code_result_message,
-                colorCode?.uppercase()
-            )
+        }
+        val backButton = findViewById<Button>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
